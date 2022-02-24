4317ea9dfdfd8cca11b98303d7ccfd58b8910344
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2022-02-24 23:01:28 +0100

    useToggle hook first version

---
+++ useToggle.js
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+const useToggle = (initialValue = false) => {
+    const [value, setValue] = useState(initialValue)
+    return [
+        flag,
+        {
+            // Most likely never needed.
+            set: setValue,
+            // The actual toggle function.
+            toggle: (event) => {
+                event.preventDefault()
+                setValue((flag) => !flag)
+            },
+        },
+    ]
+}
+
+export default useToggle
+
